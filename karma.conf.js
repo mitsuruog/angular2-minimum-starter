24f7aa6c1ef2e4351b6efa503104b961dcd93138
Ogawa Mitsuru <mitsuru.ogawa.jp@gmail.com>
2016-03-10 15:13:34 +0900

    Add coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -45,14 +45,22 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-
+      "app/**/!(*spec|*mock).js": ['coverage']
     },
 
-
+    coverageReporter: {
+      dir : 'report/coverage/',
+      reporters: [{ 
+        type: 'json', 
+        subdir : '.',
+        file : 'coverage-final.json',
+      }]
+    },
+    
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage'],
 
 
     // web server port
