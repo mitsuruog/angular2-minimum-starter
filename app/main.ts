144a6513c3f69b3bfe139888d5579687dc1ec1a7
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2016-06-23 21:18:34 +0900

    Upgrade to rc.3

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS } from '@angular/router';
 import { Logger } from './common/services/logger.service';
 
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, Logger]);
+bootstrap(AppComponent, [Logger]);
